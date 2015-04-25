36799bbaf3bb2103e7897d0c994077919debf784
Kevin Bluer <kevin@bluer.com>
2015-04-25 20:50:57 +0800

    Initial Commit

---
+++ my-where-am-i/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-where-am-i
+//
+//  Created by Kevin Bluer on 4/25/15.
+//  Copyright (c) 2015 Bluer Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
